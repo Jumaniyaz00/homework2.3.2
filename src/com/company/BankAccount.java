9b6b97e3a3abdb165836c35bb1d50a5aac0ff9f9
Jumaniyaz00 <jumaniyoz00@mail.ru>
2019-12-08 14:45:41 +0600

    dfrbrtg

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -2,11 +2,12 @@ package com.company;
 
 
 
+
 public class BankAccount{
 
    public   double amount;
 
-    public double getAmount() {
+    public double getAmount(){
         return amount;
     }
 
@@ -15,11 +16,13 @@ public class BankAccount{
         System.out.println("  ваш счет "+ amount);
         return amount;
     }
+
+
    public  void  withDraw(double sum) throws  LimitException {
         amount = amount - sum;
        System.out.println("  ваш счет после операчи "+ amount);
        if (sum> getAmount() ) {
-           throw new LimitException(" сообщение об ошибке анан счеттун остатогу   ", 88998);
+           throw new LimitException("  В вашем счоте не достаточно денег  ",amount);
        }
     }
 }
